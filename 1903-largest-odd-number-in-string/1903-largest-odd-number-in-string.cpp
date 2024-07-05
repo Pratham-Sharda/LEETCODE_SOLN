f7528e1c2a3aaa30d1b1415311d2eefa1788875c
Pratham-Sharda <144925900+Pratham-Sharda@users.noreply.github.com>
2024-07-05 21:36:51 +0530

    Time: 30 ms (39.28%), Space: 16.2 MB (50%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -6,19 +6,9 @@ public:
         for(int i=n-1;i>=0;i--){
             int val=num[i]-'0';
             if(val%2){
-                ans=i;
-                break;
+                return num.substr(0, i + 1);
             }
         }
-        string r="";
-        if(ans==-1){
-            return "";
-        }else{
-            for(int i=0;i<=ans;i++){
-                r+=num[i];
-            }
-        }
-
-        return r;
+        return "";
     }
 };
