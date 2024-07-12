323278423880a43034fb6d3e3e423c9c2bd6a0ac
Pratham-Sharda <144925900+Pratham-Sharda@users.noreply.github.com>
2024-07-12 16:55:37 +0530

    Time: 1412 ms (55.56%), Space: 7.8 MB (62.96%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
-    bool ispal(string & s,int i,int j){
-        // int i=0;
-        // int j=s.length()-1;
-        while(i<=j){
-            if(s[i]!=s[j]){
-                return false;
-            }
-            i++;j--;
-        }
-        return true;
+    bool isPalindrome(const string& s, int start, int end) {
+    // Check bounds
+    if (start < 0 || end >= s.length() || start > end) {
+        return false; // Invalid indices
     }
+    
+    // Iterate over the substring and check palindrome property
+    while (start < end) {
+        if (s[start] != s[end]) {
+            return false; // Not a palindrome
+        }
+        start++;
+        end--;
+    }
+    
+    return true; // It's a palindrome
+}
     int minCut(string s) {
         int n=s.length();
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int i=0;i<n-1;i++){
-            if(s[i]==s[i+1]){
-                dp[i][i+1]=0;
-            }else{
-                dp[i][i+1]=1;
-            }
-        }
-        for(int len=3;len<=n;len++){
-            for(int i=0;i<n-len+1;i++){
-                int j=i+len-1;
+        vector<int> dp(n+1,0);
 
-                if(ispal(s,i,j)){
-                    dp[i][j]=0;
-                    continue;
-                }else{
-                    int maxi=1e9;
-                    for(int k=i;k<j;k++){
-                        maxi=min(maxi,1+dp[i][k]+dp[k+1][j]);
-                    }
-                    dp[i][j]=maxi;
+        for(int i=n-1;i>=0;i--){
+            int mini=1e9;
+            for(int j=i;j<n;j++){
+                if(isPalindrome(s,i,j)){
+                    mini=min(1+dp[j+1],mini);
                 }
             }
+
+            dp[i]=mini;
         }
 
-        return dp[0][n-1];
-
+        return dp[0]-1;
     }
 };
